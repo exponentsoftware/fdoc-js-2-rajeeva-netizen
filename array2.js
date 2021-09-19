c2ac270567d12890a44cebb958757be7c8c40429
rajeeva-netizen <74817212+rajeeva-netizen@users.noreply.github.com>
2021-09-19 19:05:00 +0530

    updated

--- array2.js
+++ array2.js
@@ -44,4 +44,46 @@ const users = [
     ];
     
     users.filter(x=>x.scores>85)
+    let name = "preetham"
+    let newObj = {
+      name:'preetham',
+      scores:100,
+      skills:['html','css'],
+      age:25
+    }
+    let flag;
+    users.map(x=>{
+      x.name===name?flag = true:flag = false
+      
+    })
+    //console.log(flag)
+    if(flag === false){
+      users.push(newObj)
+    }
     
+    //console.log(users)
+    
+    let newFlag;
+    let newName = "Martha"
+    users.map(x=>{
+      x.name === newName?newFlag = true:newFlag=false
+      if(newFlag === true){
+        x.skills.push("react")
+      }
+      //console.log(x.skills.push("react"))
+      
+    })
+    // console.log(newFlag)
+    // console.log(users)
+    
+    
+    let editFlag;
+    let editName = "Brook"
+    users.map(x=>{
+      x.name === editName ? editFlag = true: editFlag = false
+      if(editFlag === true){
+        x.name = "newName"
+      }
+    })
+    
+    console.log(users)
